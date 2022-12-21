b96f8c6eafa78691ae507e98e3489df9586938c1
Franco Lepe <Francolepe169@gmail.com>
2022-12-21 16:43:57 -0500

    commit tree e6e95ac8fa57e475c5192fbb0ae18902724d3843

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+   
+}
+const gifts = ["teddy bear","drone","doll"];
+function wrapGifts(gifts){
+    for(let i =0; i <gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+const cards =(["Tony","Mark","Jeff"],"surprise")
+function writeCards(cards){
+    const light=[]
+    for(let i=0; i < cards.length; i++){
+        light.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+    return light;
+
+}
+writeCards(["Guadalupe","Ollie","Aki"],"cards");
+
+function countDown(num) {
+while (num >= 0) {
+  console.log(num);
+  num--
+}
+}
+
+
